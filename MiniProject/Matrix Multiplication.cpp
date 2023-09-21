5a5185641562020c2e2b6f704da81437abd34b72
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2023-09-21 21:13:29 +0530

    Create Matrix Multiplication.cpp

---
+++ MiniProject/Matrix Multiplication.cpp
@@ -0,0 +1,1 @@
+// Write a program to implement matrix multiplication. Also implement multithreaded matrix multiplication with either one thread per row or one thread per cell. Analyze and compare their performance.
